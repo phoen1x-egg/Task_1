fa22153d1dc524049c0287163912018753795d19
phoen1x-egg <rbaradulin@gmail.com>
2020-09-14 17:36:02 +0300

    14.09.2020 2

---
+++ ex01/second.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+char ft_putchar (char kk); 
+void ft_print_alphabet (void);
+
+char ft_putchar (char kk) {
+	putchar(kk);
+	return(0);
+} 
+
+void ft_print_alphabet (void) {
+         char ch;
+  	 for(ch = 'a' ; ch <= 'z' ; ch++) {
+	 ft_putchar(ch); 
+  	 }
+}
+
+int main () {   
+ft_print_alphabet();
+ return(0);
+}
+
